720e4377fa4abf00f678436b532db78a2273104d
ikshitij <48786591+ikshitij@users.noreply.github.com>
2025-03-29 13:42:41 +0530

    Update TB1.java
    removed switch case

--- src/test/java/testBase/TB1.java
+++ src/test/java/testBase/TB1.java
@@ -7,34 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TB1 {
 
-	public static WebDriver driver;
-
-	public static void main(String[] args) {
-	
-		
-		String Browser = "Chrome";
-	
-		switch (Browser) {
-	case "Chrome":
-	{		
-WebDriver driver = new ChromeDriver();
-	}
-	break;
-	case "firefox":
-	{
-WebDriver driver = new FirefoxDriver();
-	}
-	break;
-	
-	case "Edge":
-	{	
-WebDriver driver  = new EdgeDriver();
-	}
-   break;
-}
-
-
-	}
+	WebDriver driver = new ChromeDriver();
 
 }
 
